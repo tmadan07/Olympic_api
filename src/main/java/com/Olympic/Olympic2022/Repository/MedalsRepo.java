e7c21a287a06748bb6daeb772ccfed1be0df4d5f
Madan chettri <tmadan446@gmail.com>
2022-10-12 04:55:50 +0545

    lone

--- src/main/java/com/Olympic/Olympic2022/Repository/MedalsRepo.java
+++ src/main/java/com/Olympic/Olympic2022/Repository/MedalsRepo.java
@@ -1,9 +1,12 @@
 package com.Olympic.Olympic2022.Repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.Olympic.Olympic2022.Model.Medals;
+import com.Olympic.Olympic2022.Model.User;
 
 @Repository
 public interface MedalsRepo extends JpaRepository<Medals, Long> {
--- src/test/java/com/Olympic/Olympic2022/Olympic2022ApplicationTests.java
+++ src/test/java/com/Olympic/Olympic2022/Olympic2022ApplicationTests.java
@@ -1,13 +1,62 @@
 package com.Olympic.Olympic2022;
 
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.Olympic.Olympic2022.Model.Athletes;
+import com.Olympic.Olympic2022.Model.Medals;
+import com.Olympic.Olympic2022.Repository.AthletesRepo;
+import com.Olympic.Olympic2022.Repository.MedalsRepo;
 
 @SpringBootTest
 class Olympic2022ApplicationTests {
+	
+//	@Autowired
+//	AthletesRepo athletesRepository;
+//
+//	@Autowired
+//	MedalsRepo medalRepository;
+//	
+//	
+//	@Test
+//	public void Athletestest () {
+//		Athletes a = new Athletes();
+//		a.setCountry("Nepal");
+//		a.setCountry("Madan Thapa");
+//		a.setImage("C:\\Users\\Dell\\OneDrive\\Pictures\\Screenshots\\one.png");
+//		a.setSport("Football");
+//		a.setTotalMedals(10);
+//		athletesRepository.save(a);
+//		assertNotNull(athletesRepository.findById(1L).get());
+//	}
 
-	@Test
-	void contextLoads() {
-	}
+	
+	
+//	@Test
+//	public void testCreate () {
+//		Medals m = new Medals();
+//		m.setRank(4);
+//		m.setCountry("Nepal");
+//		m.setGold(5);
+//		m.setSilver(5);
+//		m.setBronze(2);
+//		m.setTotal(12);
+//		medalRepository.save(m);
+//		assertNotNull(medalRepository.findById(1L).get());
+//	}
+//	
+//	@Test
+//	public void testUpdate () {
+//		Medals m = medalRepository.findById(1L).get();
+//		m.setCountry("Bhutan");
+//		medalRepository.save(m);
+//		assertNotNull(medalRepository.findById(1L).get());
+//	}
+	
+	
 
 }
